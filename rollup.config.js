d4389a0bad8ae54f6ad4745a25ffd73a0260a1d9
larbibaraka <laridev@gmail.com>
2020-05-01 00:32:18 +0100

    adding string rollup plugin to load css and other ressources

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
+import { string } from 'rollup-plugin-string'
 const dist = 'dist'
 const production = !process.env.ROLLUP_WATCH
 export default {
@@ -28,6 +29,9 @@ export default {
     resolve(),
     babel({
       exclude: 'node_modules/**'
+    }),
+    string({
+      include: '**/*.css'
     }),
     production && terser()
   ]
